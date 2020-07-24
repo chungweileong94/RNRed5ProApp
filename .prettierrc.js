0d49c636ba3cd3339bdd9951aceaee44e86e4584
Chung Wei Leong <chungwei@whiteroom.work>
2020-07-24 10:19:27 +0800

    Setup eslint & prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,15 @@
 module.exports = {
+  arrowParens: 'avoid',
   bracketSpacing: false,
-  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: false,
 };
