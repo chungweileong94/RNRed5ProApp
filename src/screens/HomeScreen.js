af87501ba4f7496aa804058cc1efd7d8bec6328d
Chung Wei Leong <chungwei@whiteroom.work>
2020-07-27 15:44:11 +0800

    Added getLiveStream API call

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,24 @@
-import React, {useCallback, useLayoutEffect, useRef} from 'react';
-import {View, Alert} from 'react-native';
+import React, {useCallback, useLayoutEffect, useRef, useMemo} from 'react';
+import {View, Alert, Text, FlatList, StyleSheet, RefreshControl} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import IconButton from '../components/IconButton';
 import {requestPermissions, MY_PERMISSIONS} from '../utils/permissions';
 import SettingsModal from '../components/SettingsModal';
+import {useLiveStreams} from '../api';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const settingsModalRef = useRef(null);
+  const {isLoading, error, data, refetch} = useLiveStreams();
+
+  const liveStreams = useMemo(() => {
+    if (isLoading || error) {
+      return [];
+    }
+
+    return data?.data;
+  }, [data?.data, error, isLoading]);
 
   const handleOnBroadcastPress = useCallback(async () => {
     const requestResult = await requestPermissions(MY_PERMISSIONS);
@@ -34,10 +44,34 @@ const HomeScreen = () => {
 
   return (
     <>
-      <View />
+      <View style={styles.container}>
+        <FlatList
+          data={liveStreams}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <View style={styles.liveStreamItem}>
+              <Text style={styles.listStreamText}>{item}</Text>
+            </View>
+          )}
+          refreshControl={<RefreshControl refreshing={isLoading} onRefresh={refetch} />}
+        />
+      </View>
       <SettingsModal ref={settingsModalRef} />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  liveStreamItem: {
+    paddingVertical: 20,
+    paddingHorizontal: 18,
+  },
+  listStreamText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default HomeScreen;
