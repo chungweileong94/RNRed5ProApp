548d5c0f52fbe6d411729d3b7fd508953d4ce24b
Chung Wei Leong <chungwei@whiteroom.work>
2020-07-29 11:31:38 +0800

    Make getLiveStream API realtime

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,15 @@ import {useQuery} from 'react-query';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export const useLiveStreams = () =>
-  useQuery('liveStream', async () => {
-    const host = await AsyncStorage.getItem('host');
-    const res = await fetch(`http://${host}:5080/api/v1/applications/live/streams?accessToken=abc123`);
-    const data = await res.json();
-    return data;
-  });
+  useQuery(
+    'liveStream',
+    async () => {
+      const host = await AsyncStorage.getItem('host');
+      const res = await fetch(`http://${host}:5080/api/v1/applications/live/streams?accessToken=abc123`);
+      const data = await res.json();
+      return data;
+    },
+    {
+      refetchInterval: 5000,
+    },
+  );
